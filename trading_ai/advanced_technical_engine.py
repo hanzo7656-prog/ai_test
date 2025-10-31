8f4bca4272312cab486b4e6e4ea79d953fa9a7ca
hanzo7656-prog <hanzo7656@gmail.com>
2025-11-01 01:00:43 +0330

    Update advanced_technical_engine.py

--- trading_ai/advanced_technical_engine.py
+++ trading_ai/advanced_technical_engine.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import numpy as np
-from typing import Dict, List, Tuple, Optional
+from typing import Dict, List, Tuple, Optional, Any
 from dataclasses import dataclass
 import logging
 
