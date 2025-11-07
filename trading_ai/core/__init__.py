1e841a38311c43386ff1101c78cd906765d734e8
hanzo7656-prog <hanzo7656@gmail.com>
2025-11-07 13:14:14 +0330

    Create __init__.py

---
+++ trading_ai/core/__init__.py
@@ -0,0 +1,10 @@
+# ماژول core - هسته مرکزی Trading AI
+from .config import AIConfig, ai_config
+from .utils import AIUtils, ai_utils
+
+__all__ = [
+    'AIConfig',
+    'AIUtils', 
+    'ai_config',
+    'ai_utils'
+]
